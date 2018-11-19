686ce20b92232b669eac999099e61b9c4ac537a8
wpyzh <1301124775@qq.com>
2018-11-19 19:58:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a80021714/netty/DiscoveryAdapter.java
@@ -0,0 +1,57 @@
+package com.example.a80021714.netty;
+
+import java.net.InetAddress;
+
+import android.content.Intent;
+
+/**
+ * This adapter class provides empty implementations of the methods from
+ * {@link DiscoveryListener}.
+ *
+ * Any custom listener that cares only about a subset of the methods of this listener
+ * can simply subclass this adapter class instead of implementing the interface
+ * directly.
+ */
+public abstract class DiscoveryAdapter implements DiscoveryListener {
+    /**
+     * Called when the {@link Discovery} has successfully received an {@link Intent}.
+     *
+     * @param address The IP address of the sender of the {@link Intent}.
+     * @param intent The received {@link Intent}.
+     */
+    @Override
+    public abstract void onIntentDiscovered(InetAddress address, Intent intent);
+
+    /**
+     * The {@link Discovery} has been started and is now waiting for incoming
+     * {@link Intent}s.
+     *
+     * Empty default implementation.
+     */
+    @Override
+    public void onDiscoveryStarted() {
+        // Empty default implementation
+    }
+
+    /**
+     * The {@link Discovery} has been stopped.
+     *
+     * Empty default implementation.
+     */
+    @Override
+    public void onDiscoveryStopped() {
+        // Empty default implementation
+    }
+
+    /**
+     * An unrecoverable error occured. The {@link Discovery} is going to be stopped.
+     *
+     * Empty default implementation.
+     *
+     * @param exception Actual exception that occured in the background thread
+     */
+    @Override
+    public void onDiscoveryError(Exception exception) {
+        // Empty default implementation
+    }
+}
---
+++ app/src/main/java/com/example/a80021714/netty/DiscoveryListener.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2013 Sebastian Kaspari
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.a80021714.netty;
+
+import java.net.InetAddress;
+
+import android.content.Intent;
+
+/**
+ * A {@link DiscoveryListener} receives notifications from a {@link Discovery}.
+ * Notifications indicate lifecycle related events as well as successfully received
+ * {@link Intent}s.
+ */
+public interface DiscoveryListener {
+    /**
+     * The {@link Discovery} has been started and is now waiting for incoming
+     * {@link Intent}s.
+     */
+    public void onDiscoveryStarted();
+
+    /**
+     * The {@link Discovery} has been stopped.
+     */
+    public void onDiscoveryStopped();
+
+    /**
+     * An unrecoverable error occured. The {@link Discovery} is going to be stopped.
+     *
+     * @param exception Actual exception that occured in the background thread
+     */
+    public void onDiscoveryError(Exception exception);
+
+    /**
+     * Called when the {@link Discovery} has successfully received an {@link Intent}.
+     *
+     * @param address The IP address of the sender of the {@link Intent}.
+     * @param intent The received {@link Intent}.
+     */
+    public void onIntentDiscovered(InetAddress address, Intent intent);
+}
